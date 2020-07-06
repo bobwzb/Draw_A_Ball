eb293d44a94de24baf0e79a5e3eb26b39f114286
bobwzb <42678607+bobwzb@users.noreply.github.com>
2020-07-06 00:42:29 -0700

    add lambert render

--- DrawCircle/DrawCircle/DrawCircle.cpp
+++ DrawCircle/DrawCircle/DrawCircle.cpp
@@ -9,6 +9,10 @@ int main() {
 		for (x = -1; x <= 1; x += 0.025f) {
 			putchar(x*x + y * y > 1 ? 'B' : "&@%#*+=;:. "[(int)(
 				((x + y + sqrt(1 - (x*x + y * y)))*sqrt(3)/3 + 1)*5.5f)]);//Half Lambert Render
+			/* Lambert Render
+			putchar(x*x + y * y > 1 ? 'B' : "&@%#*+=;:. "[(int)(
+				((x + y + sqrt(1 - (x*x + y * y)))*sqrt(3)/3 )*11)]);
+			*/
 		}
 	}
 }
